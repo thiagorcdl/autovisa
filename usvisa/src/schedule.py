4f5162195055efac9c17751399e702c60fee944f
Thiago Lima <thiagorcdl@gmail.com>
2022-08-30 13:26:17 -0230

    add Scheduler structure

--- usvisa/src/schedule.py
+++ usvisa/src/schedule.py
@@ -5,38 +5,70 @@ import logging
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-from usvisa.src.constants import DEFAULT_WEBDRIVER
+from usvisa.src.constants import DEFAULT_WEBDRIVER, LOGIN_URL
 
 logger = logging.getLogger()
 
 
+class Appointment:
+    city = None
+    date = None
+    time = None
+
+
 class Scheduler:
+
+    driver = None
+    current_appointment: Appointment = None
+    new_appointment: Appointment = None
 
     def __init__(self):
         self.driver = DEFAULT_WEBDRIVER()
 
     def navigate_login_page(self):
-        # TODO implement
+        self.driver.get(LOGIN_URL)
         return
 
     def execute_login(self):
-        # TODO implement
+        # Find email field
+        # Fill in email field
+        # Find pwd field
+        # Fill in pwd field
+        # Find CTA
+        # Click CTA
         return
 
     def get_current_appointment(self):
-        # TODO implement
+        # Find appointment element
+        # Retrieve text
+        # Parse text
+        # Store in attribute
         return
 
     def navigate_reschedule_page(self):
-        # TODO implement
+        # Find "continue" CTA
+        # Click CTA
+        # Find "reschedule" section
+        # Expand "reschedule" section
+        # Find "reschedule" CTA
+        # Click CTA
         return
 
     def get_best_date(self):
-        # TODO implement
-        return
+        # Find dropdown of cities
+        # For each city
+            # Select city
+            # TODO find out if possible: get json results
+            # get first date
+            # compare with self.current_appointment
+            # set self.new_appointment
+        return self.new_appointment
 
     def execute_reschedule(self):
-        # TODO implement
+        # Find dropdown of cities
+        # Select self.new_appointment.city
+        # Find date field
+        # Find calendar
         return
 
     def reschedule_sooner(self):
@@ -46,5 +78,6 @@ class Scheduler:
         self.get_current_appointment()
         self.navigate_reschedule_page()
         self.get_best_date()
-        self.execute_reschedule()
+        if self.new_appointment.date < self.current_appointment:
+            self.execute_reschedule()
         return
